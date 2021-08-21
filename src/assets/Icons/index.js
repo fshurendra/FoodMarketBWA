70066eb8552fdbe0fa34fc911848094dd6bc59e0
Fauzi Shurendra <fshurendra@gmail.com>
2021-08-21 10:48:47 +0700

    CustomBottomTabNavigator

--- src/assets/Icons/index.js
+++ src/assets/Icons/index.js
@@ -1,3 +1,17 @@
 import IcBack from '../Icons/Ic_arrow_back.svg';
+import IcHomeOn from '../Icons/ic_home_on.svg';
+import IcHomeOff from '../Icons/ic_home_off.svg';
+import IcOrderOn from '../Icons/ic_order_on.svg';
+import IcOrderOff from '../Icons/ic_order_off.svg';
+import IcProfileOn from '../Icons/ic_profile_on.svg';
+import IcProfileOff from '../Icons/ic_profile_off.svg';
 
-export {IcBack};
+export {
+  IcBack,
+  IcHomeOn,
+  IcHomeOff,
+  IcOrderOn,
+  IcOrderOff,
+  IcProfileOn,
+  IcProfileOff,
+};
